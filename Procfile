1d74296366bb6173f289f91aa143b26f8f221c4c
Ayush-Malik <ayushmalik779@gmail.com>
2020-06-22 12:59:49 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fun.wsgi --log-file -
---
+++ new_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from new_app.models import Contact
+
+admin.site.register(Contact)
